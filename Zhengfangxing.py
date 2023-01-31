56b9ccf860c649b7afdf96cbe6294eb3a67352b7
Andyli-1020 <andyli1020@outlook.com>
2023-01-31 15:41:43 +0000

    angle4

--- Zhengfangxing.py
+++ Zhengfangxing.py
@@ -69,13 +69,13 @@ def curve(distance, degree, timing):
 
 try:
     go(40, 3)
-    rot(110, 3)
+    rot(105, 3)
     go(40, 3)
-    rot(110, 3)
+    rot(105, 3)
     go(40, 3)
-    rot(110, 3)
+    rot(105, 3)
     go(40, 3)
-    rot(110, 3)
+    rot(105, 3)
 except:
     print('error')
 
