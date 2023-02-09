211fbdae74b0de235b841cbdf114f35768fc93da
GuoYipeng <yg2719@ic.ac.uk>
2023-02-09 16:57:30 +0000

    main

--- Zhengfangxing.py
+++ Zhengfangxing.py
@@ -95,115 +95,116 @@ def curve(distance, degree, timing):
             break
         time.sleep(0.02)
 
+if __name__ == '__main__':
 # print('something')
-try:
-    go(40, 3)
-    rot(90, 3)
-    go(40, 3)
-    rot(90, 3)
-    go(40, 3)
-    rot(90, 3)
-    go(40, 3)
-    rot(90, 3)
-except:
-    BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
-    BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
-    print('error')
-    BP.reset_all()
+    try:
+        go(40, 3)
+        rot(90, 3)
+        go(40, 3)
+        rot(90, 3)
+        go(40, 3)
+        rot(90, 3)
+        go(40, 3)
+        rot(90, 3)
+    except:
+        BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
+        BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
+        print('error')
+        BP.reset_all()
 
-# try:
-#     for i in range(4):
-#         for i in range(4):
-#             go(10, 1)
-#             particle_list = []
-#             for particle in particles:
-#                 e = random.gauss(0, e_sigma)
-#                 f = random.gauss(0, f_sigma)
-            
-#                 particle[0] += (d+e)*math.cos(particle[2])
-#                 particle[1] += (d+e)*math.sin(particle[2])
-#                 particle[2] += f
-#                 particle_tuple = (particle[0], particle[1], particle[2])
-#                 particle_list.append(particle_tuple)
-       
-#             # print ("drawParticles:" + str(tuple(particle_list)))
-#             time.sleep(5)
-#         rot(90, 3)
-#         particle_list = []
-#         for particle in particles:
-#             g = random.gauss(0, g_sigma)
-#             particle[2] += 90 + g
-#             particle_tuple = (particle[0], particle[1], particle[2])
-#             particle_list.append(particle_tuple)
-       
-#     # print ("drawParticles:" + str(tuple(particle_list)))
+    # try:
+    #     for i in range(4):
+    #         for i in range(4):
+    #             go(10, 1)
+    #             particle_list = []
+    #             for particle in particles:
+    #                 e = random.gauss(0, e_sigma)
+    #                 f = random.gauss(0, f_sigma)
+                
+    #                 particle[0] += (d+e)*math.cos(particle[2])
+    #                 particle[1] += (d+e)*math.sin(particle[2])
+    #                 particle[2] += f
+    #                 particle_tuple = (particle[0], particle[1], particle[2])
+    #                 particle_list.append(particle_tuple)
+        
+    #             # print ("drawParticles:" + str(tuple(particle_list)))
+    #             time.sleep(5)
+    #         rot(90, 3)
+    #         particle_list = []
+    #         for particle in particles:
+    #             g = random.gauss(0, g_sigma)
+    #             particle[2] += 90 + g
+    #             particle_tuple = (particle[0], particle[1], particle[2])
+    #             particle_list.append(particle_tuple)
+        
+    #     # print ("drawParticles:" + str(tuple(particle_list)))
 
-#     time.sleep(2)
+    #     time.sleep(2)
 
-# except:
-#     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
-#     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
-#     print('error')
-#     BP.reset_all()
+    # except:
+    #     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
+    #     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
+    #     print('error')
+    #     BP.reset_all()
 
-# try:
-#     while True:
-#         print(1)
-#         left, right = 0,0
-#         inp = input()
-#         print(inp)
-#         if inp == w:
-#             print(3)
-#             left -= 100
-#             right -= 100
-#         if inp == s:
-#             left += 100
-#             right += 100
-#         if inp == a:
-#             left -= 100
-#             right += 100
-#         if inp == d:
-#             left += 100
-#             right -= 100
-#         print(7)
-#         BP.set_motor_dps(BP.PORT_B, left)
-#         BP.set_motor_dps(BP.PORT_D, right)
-#         time.sleep(0.02)
-# except Exception:
-#     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
-#     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
-#     print(Exception)
-    
+    # try:
+    #     while True:
+    #         print(1)
+    #         left, right = 0,0
+    #         inp = input()
+    #         print(inp)
+    #         if inp == w:
+    #             print(3)
+    #             left -= 100
+    #             right -= 100
+    #         if inp == s:
+    #             left += 100
+    #             right += 100
+    #         if inp == a:
+    #             left -= 100
+    #             right += 100
+    #         if inp == d:
+    #             left += 100
+    #             right -= 100
+    #         print(7)
+    #         BP.set_motor_dps(BP.PORT_B, left)
+    #         BP.set_motor_dps(BP.PORT_D, right)
+    #         time.sleep(0.02)
+    # except Exception:
+    #     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
+    #     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
+    #     print(Exception)
+        
 
 
-# try:
-#     go(-40, 3)
-# except:
-#     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
-#     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
-#     print('error')
+    # try:
+    #     go(-40, 3)
+    # except:
+    #     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
+    #     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
+    #     print('error')
 
-# try:
-#     curve(50, 100, 6)
-#     curve(50, -100, 6)
-#     curve(50, 100, 6)
-#     curve(50, -100, 6)
-#     curve(50, 100, 6)
-#     curve(50, -100, 6)
-#     curve(50, 100, 6)
-#     curve(50, -100, 6)
-# except:
-#     print('error')
+    # try:
+    #     curve(50, 100, 6)
+    #     curve(50, -100, 6)
+    #     curve(50, 100, 6)
+    #     curve(50, -100, 6)
+    #     curve(50, 100, 6)
+    #     curve(50, -100, 6)
+    #     curve(50, 100, 6)
+    #     curve(50, -100, 6)
+    # except:
+    #     print('error')
 
-# BP.set_motor_position(BP.PORT_B,-800)
-# BP.set_motor_position(BP.PORT_D,-800)
-# BP.set_motor_dps(BP.PORT_B,-100)
-# BP.set_motor_dps(BP.PORT_D,-100)
-'''
-    B=BP.get_motor_encoder(BP.PORT_B)
-    D=BP.get_motor_encoder(BP.PORT_D)
-    print('B position: ',B,'D position: ',D)'''
-if KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
-    BP.reset_all()
-else:
-    pass
+    # BP.set_motor_position(BP.PORT_B,-800)
+    # BP.set_motor_position(BP.PORT_D,-800)
+    # BP.set_motor_dps(BP.PORT_B,-100)
+    # BP.set_motor_dps(BP.PORT_D,-100)
+    '''
+        B=BP.get_motor_encoder(BP.PORT_B)
+        D=BP.get_motor_encoder(BP.PORT_D)
+        print('B position: ',B,'D position: ',D)'''
+    if KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
+        BP.reset_all()
+    else:
+        pass
