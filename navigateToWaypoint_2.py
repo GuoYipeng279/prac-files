e074b8fafb42c47ed35bb15a08f2dacbd7830887
LIJIAN1998 <844299030@qq.com>
2023-02-16 16:00:09 +0000

    prob

--- navigateToWaypoint_2.py
+++ navigateToWaypoint_2.py
@@ -14,6 +14,25 @@ g_sigma = 0.01
 d = 10*scale
 total_particles = 100.0
 
+point_O = (0, 0)
+point_A = (0, 168)
+point_B = (84, 168)
+point_C = (84, 126)
+point_D = (84, 210)
+point_E = (168, 210)
+point_F = (168, 84)
+point_G = (210, 84)
+point_H = (210, 0)
+wall_a = (point_O, point_A)
+wall_b = (point_A, point_B)
+wall_c = (point_C, point_D)
+wall_d = (point_D, point_E)
+wall_e = (point_E, point_F)
+wall_f = (point_F, point_G)
+wall_g = (point_G, point_H)
+wall_h = (point_H, point_O)
+walls = [wall_a, wall_b, wall_c, wall_d, wall_e, wall_f, wall_g, wall_h]
+
 particles = np.zeros([100, 4])
 
 particles += [0+displacement*scale, 40*scale+displacement*scale, 0, 1/total_particles]
@@ -59,6 +78,12 @@ def navigateToWaypoint(X, Y):
         g = random.gauss(0, current_g_sigma)
 
         particle[2] += beta + g
+        z = measure
+        prob = calculate_likelihood(particle[0]/scale - displacement, 
+                                    scale+displacement - particle[1]/scale, 
+                                    particle[2],
+                                    z)
+        particle[3] *= prob
         particle_tuple = (particle[0], particle[1], particle[2])
         particle_list.append(particle_tuple)
     # print ("drawParticles:" + str(tuple(particle_list)))
@@ -80,10 +105,17 @@ def navigateToWaypoint(X, Y):
         particle[0] += (distance+e)*math.cos(particle[2])
         particle[1] += (distance+e)*math.sin(particle[2])
         particle[2] += f
+        z = measure
+        prob = calculate_likelihood(particle[0]/scale - displacement, 
+                                    scale+displacement - particle[1]/scale, 
+                                    particle[2],
+                                    z)
+        particle[3] *= prob
         particle_tuple = (particle[0], particle[1], particle[2])
         particle_list.append(particle_tuple)
     # print ("drawParticles:" + str(tuple(particle_list)))
     time.sleep(3)
+    
     sum_x, sum_y, sum_deg = 0, 0, 0
     for particle in particles:
         sum_x += particle[0] * particle[3]
@@ -92,6 +124,30 @@ def navigateToWaypoint(X, Y):
     robot_position = [sum_x, sum_y, sum_deg]
     print(robot_position)
     print(robot_position[0]/scale - displacement, displacement + 40 - robot_position[1]/scale, robot_position[2])
+
+def calculate_likelihood(x, y, theta, z):
+    std_sensor = 1
+    K = 0
+    candidate_walls = []
+    candidate_m = []
+    for wall in walls:
+        p1 = wall[0]
+        p2 = wall[1]
+        m = ((p2[1]-p1[1]) * (p1[0]-x) - (p2[0]-p1[0])*(p1[1]-y)) /  \
+            ((p2[1]-p1[1])*math.cos(theta) - (p2[0]-p1[0])*math.sin(theta))
+        if min(p1[0], p2[0]) < x + m * math.cos(theta) < max(p1[0], p2[0]) and \
+            min(p1[1], p2[1]) < y + m * math.sin(theta) < max(p1[1], p2[1]):
+            candidate_walls.append(wall)
+            candidate_m.append(m)
+    target_index = np.argmin(candidate_m)
+    target_wall = candidate_walls[target_index]
+    target_m = candidate_m[target_index]
+    probability = math.e ** (-(z - target_m)**2 / (2*std_sensor**2)) + K
+    return probability
+
+
+
+
 
 BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder A
 BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
