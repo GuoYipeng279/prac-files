60f1ba5e8eb925bedeb3827523002ac2f243ea76
LIJIAN1998 <844299030@qq.com>
2023-02-21 21:43:42 +0000

    deleterot

--- navigateToWaypoint.py
+++ navigateToWaypoint.py
@@ -71,7 +71,6 @@ def navigateToWaypoint(X, Y):
     beta = alpha - robot_position[2]
     # print(beta * 180 / math.pi)
     # rot(-beta * 180 / math.pi, 30)
-    rot(-beta * 180 / math.pi, 30)
     particle_list = []
     for particle in particles:
         current_g_sigma = g_sigma * (alpha / (-math.pi/2))
