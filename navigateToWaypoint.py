072d3c02a2adc36e77038ec29fa3a82c735b9af8
LIJIAN1998 <844299030@qq.com>
2023-02-21 18:19:55 +0000

    nav

--- navigateToWaypoint.py
+++ navigateToWaypoint.py
@@ -6,22 +6,22 @@ import brickpi3
 BP = brickpi3.BrickPi3()
 
 BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
-try:
-    while True:
-        # read and display the sensor value
-        # BP.get_sensor retrieves a sensor value.
-        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
-        # BP.get_sensor returns the sensor value (what we want to display).
-        try:
-            value = BP.get_sensor(BP.PORT_1)
-            print(value)                         # print the distance in CM
-        except brickpi3.SensorError as error:
-            print(error)
+# try:
+#     while True:
+#         # read and display the sensor value
+#         # BP.get_sensor retrieves a sensor value.
+#         # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
+#         # BP.get_sensor returns the sensor value (what we want to display).
+#         try:
+#             value = BP.get_sensor(BP.PORT_1)
+#             print(value)                         # print the distance in CM
+#         except brickpi3.SensorError as error:
+#             print(error)
         
-        time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
+#         time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
 
-except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
-    BP.reset_all()
+# except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+#     BP.reset_all()
 
 scale = 10
 displacement = 10
@@ -64,3 +64,14 @@ print ("drawLine:" + str((-5*scale+displacement*scale, 30*scale+(displacement+5)
 
 print ("drawLine:" + str((-5*scale+displacement*scale, 40*scale+(displacement+5)*scale, -5*scale+displacement*scale, 30*scale+(displacement+5)*scale)))
 print ("drawLine:" + str((5*scale+10*scale, 40*scale+(displacement+5)*scale, 5*scale+10*scale, 30*scale+(displacement+5)*scale)))
+
+def navigateToWaypoint(X, Y):
+    while True:
+        try:
+            v = BP.get_sensor(BP.PORT_1)
+            print(v)                         # print the distance in CM
+        except brickpi3.SensorError as error:
+            print(error)
+        time.sleep(0.1)
+
+navigateToWaypoint(11,1)
