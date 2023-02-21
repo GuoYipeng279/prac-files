07d0831b9228ba2a607647377034b186ec3134e1
LIJIAN1998 <844299030@qq.com>
2023-02-21 19:56:30 +0000

    drawmap

--- navigateToWaypoint_2.py
+++ navigateToWaypoint_2.py
@@ -4,20 +4,21 @@ import numpy as np
 import random
 from Zhengfangxing import go, rot
 import brickpi3 
+from particleDataStructures import Map
 BP = brickpi3.BrickPi3()
 
 BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
-# while True:
-#         # read and display the sensor value
-#         # BP.get_sensor retrieves a sensor value.
-#         # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
-#         # BP.get_sensor returns the sensor value (what we want to display).
-#         try:
-#             value = BP.get_sensor(BP.PORT_1)
-#             print(value)                         # print the distance in CM
-#         except brickpi3.SensorError as error:
-#             print(error)
-#         time.sleep(0.1)
+while True:
+        # read and display the sensor value
+        # BP.get_sensor retrieves a sensor value.
+        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
+        # BP.get_sensor returns the sensor value (what we want to display).
+        try:
+            value = BP.get_sensor(BP.PORT_1)
+            print(value)                         # print the distance in CM
+        except brickpi3.SensorError as error:
+            print(error)
+        time.sleep(0.1)
 
 scale = 10
 displacement = 10
