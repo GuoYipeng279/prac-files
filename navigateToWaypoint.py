3c7e719f06a08395a4a71c8e8fed863bfc9396ee
GuoYipeng <yg2719@ic.ac.uk>
2023-02-22 23:53:55 +0000

    new

--- navigateToWaypoint.py
+++ navigateToWaypoint.py
@@ -171,13 +171,13 @@ def calculate_likelihood(x, y, theta, z):
         if abs((p2[1]-p1[1])*math.cos(theta) - (p2[0]-p1[0])*math.sin(theta)) > 1e-2:
             m = ((p2[1]-p1[1]) * (p1[0]-x) - (p2[0]-p1[0])*(p1[1]-y)) /  \
                 ((p2[1]-p1[1])*math.cos(theta) - (p2[0]-p1[0])*math.sin(theta))
-            print('m:',m,' theta:',theta,' x:',x,' y:',y, ' z:',z)
-            print("min x: ", min(p1[0], p2[0]))
-            print("max x: ", max(p1[0], p2[0]))
-            print(x + m * math.cos(theta))
-            print("min y: ", min(p1[1], p2[1]))
-            print("max y: ", max(p1[1], p2[1]))
-            print(y + m * math.sin(theta))
+            # print('m:',m,' theta:',theta,' x:',x,' y:',y, ' z:',z)
+            # print("min x: ", min(p1[0], p2[0]))
+            # print("max x: ", max(p1[0], p2[0]))
+            # print(x + m * math.cos(theta))
+            # print("min y: ", min(p1[1], p2[1]))
+            # print("max y: ", max(p1[1], p2[1]))
+            # print(y + m * math.sin(theta))
             if m > 0 and min(p1[0], p2[0]) <= x + m * math.cos(theta) <= max(p1[0], p2[0]) and \
                 min(p1[1], p2[1]) <= y + m * math.sin(theta) <= max(p1[1], p2[1]):
                 candidate_walls.append(wall)
